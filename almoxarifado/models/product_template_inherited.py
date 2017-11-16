e2965e029e6c62897ccbf1993490ad8ab1d414af
edsonjunior <edsonjunior@edson-junior>
2017-11-16 16:44:20 -0300

    Adicionando campo origem das compras no produto

--- almoxarifado/models/product_template_inherited.py
+++ almoxarifado/models/product_template_inherited.py
@@ -8,3 +8,8 @@ class Product_template_inherited(models.Model):
         selection=[('sim', 'Sim'), ('nao', 'Não')],
         string='Imobilizado',
         help='Este produto consta como ativo imobilizado da empresa?')
+    
+    origem_compra = fields.Many2one(
+        string='Origem das Compras',
+        help='Principal estado de origem das compras',
+        comodel_name='res.country.state')
