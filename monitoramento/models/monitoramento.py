633c203c34d548c2e20fd0981beac6fed8128d08
edsonjunior <edsonjunior@10.1.201.105>
2018-02-21 10:47:15 -0300

    Ajustes no modulo de monitoramento

--- monitoramento/models/monitoramento.py
+++ monitoramento/models/monitoramento.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields, api
+from openerp import api
+from openerp import fields
+from openerp import models
 
 class Monitoramento(models.Model):
     _name = 'monitoramento'
@@ -22,83 +24,93 @@ class Monitoramento(models.Model):
         'stage_id': _read_group_stage_ids,
     }
     
-    _defaults={
-        'stage_id' : 1
+    _defaults = {
+        'stage_id': 1
     }
     
     
     name = fields.Char(
-        string="Name",
-        size=250,
-        required=True,
-        track_visibility='onchange')
+                       string="Name",
+                       size=250,
+                       required=True,
+                       track_visibility='onchange')
     
     sistema = fields.Many2one(
-        comodel_name='sistemas_monitoramento',
-        string='Tipo',
-        help='Sistema monitorado',
-        track_visibility='onchange')
+                              comodel_name='sistemas_monitoramento',
+                              string='Tipo',
+                              help='Sistema monitorado',
+                              required=True,
+                              track_visibility='onchange')
     
     descricao = fields.Text(
-        string="Descricao",
-        help="Descreva o problema",
-        track_visibility='onchange')
+                            string="Descricao",
+                            help="Descreva o problema",
+                            required=True,
+                            track_visibility='onchange')
     
     stage_id = fields.Many2one(
-        comodel_name='estagios_monitoramento',
-        string='Status',
-        help='Status do suporte',
-        track_visibility='onchange')
+                               comodel_name='estagios_monitoramento',
+                               string='Status',
+                               help='Status do suporte',
+                               required=True,
+                               track_visibility='onchange')
     
     color = fields.Integer(
-        string="Color Index")
+                           string="Color Index")
     
     prioridade = fields.Selection(
-        selection=[('0', 'Nenhuma'),
-                   ('1', 'Muito Baixa'),
-                   ('2', 'Baixa'),
-                   ('3', 'Media'),
-                   ('4', 'Alta'),
-                   ('5', 'Muito Alta')],
-        string='Prioridade',
-        help='Prioridade do suporte',
-        track_visibility='onchange')
+                                  selection=[('0', 'Nenhuma'),
+                                  ('1', 'Muito Baixa'),
+                                  ('2', 'Baixa'),
+                                  ('3', 'Media'),
+                                  ('4', 'Alta'),
+                                  ('5', 'Muito Alta')],
+                                  string='Prioridade',
+                                  help='Prioridade do suporte',
+                                  required=True,
+                                  track_visibility='onchange')
     
     data_previsao = fields.Datetime(
-        string='Previsao de Conclusao',
-        help='Data prevista para a conclusão do suporte',
-        track_visibility='onchange')
+                                    string='Previsao de Conclusao',
+                                    help='Data prevista para a conclusão do suporte',
+                                    track_visibility='onchange')
     
     data_conclusao = fields.Datetime(
-        string='Conclusao',
-        help='Data de conclusão do suporte',
-        track_visibility='onchange')
+                                     string='Conclusao',
+                                     help='Data de conclusão do suporte',
+                                     track_visibility='onchange')
+        
+    data_contornado = fields.Datetime(
+                                      string='Contornado em',
+                                      help='Data em que o problema foi contornado paliativamente',
+                                      track_visibility='onchange')
     
     responsavel = fields.Many2one(
-        comodel_name='res.users',
-        string='Responsavel',
-        help='Responsável pela resolução do problema',
-        track_visibility='onchange')
+                                  comodel_name='res.users',
+                                  string='Responsavel',
+                                  help='Responsável pela resolução do problema',
+                                  track_visibility='onchange')
     
     exibir_site = fields.Selection(
-        selection=[('0', 'Não'),
-                   ('1', 'Sim')],
-        string='Exibir no Site',
-        help='Exibir o monitoramento no site?',
-        track_visibility='onchange')
+                                   selection=[('0', 'Não'),
+                                   ('1', 'Sim')],
+                                   string='Exibir no Site',
+                                   help='Exibir o monitoramento no site?',
+                                   required=True,
+                                   track_visibility='onchange')
     
     atrasado = fields.Selection(
-        selection=[('0', 'Não'),
-                   ('1', 'Sim')],
-        string='Atrasado',
-        help='Previsão de resolução foi ultrapassada?',
-        track_visibility='onchange')
+                                selection=[('0', 'Não'),
+                                ('1', 'Sim')],
+                                string='Atrasado',
+                                help='Previsão de resolução foi ultrapassada?',
+                                track_visibility='onchange')
     
     torre = fields.Many2many(
-        comodel_name='torre',
-        string='Torre',
-        help='Torres atingidas pelo problema',
-        track_visibility='onchange')
+                             comodel_name='torre',
+                             string='Torre',
+                             help='Torres atingidas pelo problema',
+                             track_visibility='onchange')
     
     
     def verificar_atrasos(self, cr, uid, context=None):
@@ -123,16 +135,136 @@ class Monitoramento(models.Model):
         monitoramento_obj = self.pool.get('monitoramento')
         monitoramento_ids = self.pool.get('monitoramento').search(cr, uid, [])   
         
-        for monitoramento_id in monitoramento_ids :
-            monitoramento_line = monitoramento_obj.browse(cr, uid,monitoramento_id ,context=context)
+        for monitoramento_id in monitoramento_ids:
+            monitoramento_line = monitoramento_obj.browse(cr, uid, monitoramento_id, context=context)
             previsao = datetime.strptime(monitoramento_line.data_previsao, '%Y-%m-%d %H:%M:%S')
 
             if previsao < datetime.today():
-                if monitoramento_line.stage_id.id != 4:
+                if monitoramento_line.stage_id.id != 5:
                     monitoramento_line.write({'atrasado': '1', 'color': 2}, context=context)
         return True
     
     
+    def marcar_em_tratamento(self, cr, uid, ids, context=None):
+        '''
+            Descrição:
+              Esta função tem como objetivo setar ticket como em tratamento.
+        
+            Utilização:
+              marcar_em_tratamento()
+        
+            Parâmetros:
+              cr
+                Cursor do banco de dados
+              uid
+                Usuário do sistema
+              ids
+                IDs do ticket em questão
+              context
+                Contexto atual
+        '''
+        if not context:
+            context = {}
+        
+        monitoramento_obj = self.pool.get('monitoramento')
+        monitoramento_obj.write(cr, uid, ids[0], {'stage_id': 2}, context=context)     
+        
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload',
+        }
+        
+    def marcar_em_testes(self, cr, uid, ids, context=None):
+        '''
+            Descrição:
+              Esta função tem como objetivo setar ticket como em testes.
+        
+            Utilização:
+              marcar_em_testes()
+        
+            Parâmetros:
+              cr
+                Cursor do banco de dados
+              uid
+                Usuário do sistema
+              ids
+                IDs do ticket em questão
+              context
+                Contexto atual
+        '''
+        if not context:
+            context = {}
+        
+        monitoramento_obj = self.pool.get('monitoramento')
+        monitoramento_obj.write(cr, uid, ids[0], {'stage_id': 3}, context=context)     
+        
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload',
+        }
+        
+    def marcar_contornado(self, cr, uid, ids, context=None):
+        '''
+            Descrição:
+              Esta função tem como objetivo setar ticket como contornado.
+        
+            Utilização:
+              marcar_contornado()
+        
+            Parâmetros:
+              cr
+                Cursor do banco de dados
+              uid
+                Usuário do sistema
+              ids
+                IDs do ticket em questão
+              context
+                Contexto atual
+        '''
+        from datetime import datetime
+        if not context:
+            context = {}
+        
+        monitoramento_obj = self.pool.get('monitoramento')
+        monitoramento_obj.write(cr, uid, ids[0], {'stage_id': 4, 'data_contornado': datetime.today()}, context=context)     
+        
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload',
+        }
+        
+        
+    def marcar_concluido(self, cr, uid, ids, context=None):
+        '''
+            Descrição:
+              Esta função tem como objetivo setar ticket como concluido.
+        
+            Utilização:
+              marcar_concluido()
+        
+            Parâmetros:
+              cr
+                Cursor do banco de dados
+              uid
+                Usuário do sistema
+              ids
+                IDs do ticket em questão
+              context
+                Contexto atual
+        '''
+        from datetime import datetime
+        if not context:
+            context = {}
+        
+        monitoramento_obj = self.pool.get('monitoramento')
+        monitoramento_obj.write(cr, uid, ids[0], {'stage_id': 5, 'data_conclusao': datetime.today()}, context=context)     
+        
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload',
+        }
+            
+            
     def pegar_ticket(self, cr, uid, ids, context=None):
         '''
             Descrição:
@@ -158,14 +290,14 @@ class Monitoramento(models.Model):
         monitoramento_obj.write(cr, uid, ids[0], {'responsavel': uid}, context=context)     
         
         return {
-                'type': 'ir.actions.client',
-                'tag': 'action_warn',
-                'name': 'Warning',
-                'params': {
-                    'title': 'Operação concluída com sucesso!',
-                    'text': 'Você foi selecionado como responsável pelo atendimento deste ticket.',
-                    'sticky': False
-                    }
+            'type': 'ir.actions.client',
+            'tag': 'action_warn',
+            'name': 'Warning',
+            'params': {
+                'title': 'Operação concluída com sucesso!',
+                'text': 'Você foi selecionado como responsável pelo atendimento deste ticket.',
+                'sticky': False
+                }
             }
     
     
