2018bf5e4532e472fea3394eee7aba7fd16c8a22
edsonjunior <edsonjunior@10.1.201.105>
2018-03-28 12:09:30 -0300

    Suporte dos licitados com integracao com agenda e goon

--- goon/models/chamado_goon.py
+++ goon/models/chamado_goon.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+import json
 from openerp import api
 from openerp import fields
 from openerp import models
+from suds.client import Client
 
 class Chamado_goon(models.Model):
     _name = 'chamado_goon'
@@ -95,82 +97,96 @@ class Chamado_goon(models.Model):
         
         if obj_chamado_goon.name:
             if obj_chamado_goon.tecnico.id == obj_agenda_tecnicos.name.id and obj_chamado_goon.data == obj_agenda_tecnicos.data:
-                retorno = json.loads(client.service.GetAcompanhamentoOrdemServico(
-                                     'B2754231E374B8DAB17B09DA83E40AF1354D3EFD', 
-                                     obj_companhia.client_code, 
-                                     obj_chamado_goon.id))
-
-                if retorno['success']:
-                    obj_chamado_goon.write({'status': retorno['status']})
-                    return retorno['status']
+                return self.acompanharOrdemServico(obj_companhia, obj_chamado_goon, obj_agenda_tecnicos)
             else:
-                retorno = json.loads(client.service.SetAcompanhamentoOrdemServico(
-                                     'A7E43A2C4974A8282B6E6754AF5F06DCA0DE988B', 
-                                     obj_companhia.client_code, 
-                                     obj_chamado_goon.id,
-                                     2,
-                                     obj_agenda_tecnicos.name.codigo_usuario,
-                                     str(obj_agenda_tecnicos.data).replace(" ", "T"),
-                                     obj_chamado_goon.status,
-                                     'true',
-                                     'Internal',
-                                     0))
-
-                if retorno['success']:
-                    obj_chamado_goon.write({'data': obj_agenda_tecnicos.data, 'tecnico': obj_agenda_tecnicos.name.id})
-                    return obj_chamado_goon.status
+                self.acompanharOrdemServico(obj_companhia, obj_chamado_goon, obj_agenda_tecnicos)
+                return self.editarOrdemServico(obj_companhia, obj_chamado_goon, obj_agenda_tecnicos)
         else:
-            retorno = json.loads(client.service.OpenOrdemServico(
-                                 '88E9F98B63F15D7F635B0D6027DC4443EE44B50F', 
-                                 obj_companhia.client_code, 
-                                 obj_chamado_goon.id, 
-                                 obj_chamado_goon.ocorrencia.contrato.partner_id.id_syncron, 
-                                 obj_chamado_goon.tipo_servico.id, 
-                                 '0',
-                                 str(obj_chamado_goon.data).replace(" ", "T"), 
-                                 'N', 
-                                 'Desconhecido', 
-                                 obj_chamado_goon.ocorrencia.contrato.partner_id.phone, 
-                                 obj_chamado_goon.localidade.logradouro, 
-                                 obj_chamado_goon.localidade.numero, 
-                                 obj_chamado_goon.localidade.complemento, 
-                                 obj_chamado_goon.localidade.bairro, 
-                                 obj_chamado_goon.localidade.cidade.name, 
-                                 obj_chamado_goon.localidade.estado.code, 
-                                 obj_chamado_goon.localidade.cep, 
-                                 obj_chamado_goon.observacao, 
-                                 obj_chamado_goon.localidade.latitude, 
-                                 obj_chamado_goon.localidade.longitude, 
-                                 str(obj_chamado_goon.create_date).replace(" ", "T"), 
-                                 '',
-                                 0,
-                                 '0',
-                                 obj_chamado_goon.tecnico.codigo_usuario,
-                                 str(obj_chamado_goon.data).replace(" ", "T"),
-                                 '',
-                                 'Internal'))
-
-            if retorno['success']:
-                obj_chamado_goon.write({'name': retorno['numeroOS'],'status': retorno['DESP']})
-                return {
-                    'type': 'ir.actions.client',
-                    'tag': 'action_warn',
-                    'name': 'Warning',
-                    'params': {
-                        'title': 'Sincronização Concluída!',
-                        'text': 'Dados enviados para o Goon.',
-                        'sticky': True
-                        }
-                }
-            else:
-                return {
-                    'type': 'ir.actions.client',
-                    'tag': 'action_warn',
-                    'name': 'Warning',
-                    'params': {
-                        'title': 'Erro ao sincronizar!',
-                        'text': 'Houve um erro ao sincronizar, contate o administrador do sistema.',
-                        'sticky': False
-                        }
-                }
+            return self.abrirOrdemServico(obj_companhia, obj_chamado_goon)
     
+    
+    def abrirOrdemServico(self, obj_companhia, obj_chamado_goon):
+        client = Client(obj_companhia.url)
+        retorno = json.loads(client.service.OpenOrdemServico(
+                             '88E9F98B63F15D7F635B0D6027DC4443EE44B50F', 
+                             obj_companhia.client_code, 
+                             obj_chamado_goon.id, 
+                             obj_chamado_goon.ocorrencia.contrato.partner_id.id_syncron, 
+                             obj_chamado_goon.tipo_servico.id, 
+                             '0',
+                             str(obj_chamado_goon.data).replace(" ", "T"), 
+                             'N', 
+                             'Desconhecido', 
+                             obj_chamado_goon.ocorrencia.contrato.partner_id.phone, 
+                             obj_chamado_goon.localidade.logradouro, 
+                             obj_chamado_goon.localidade.numero, 
+                             obj_chamado_goon.localidade.complemento, 
+                             obj_chamado_goon.localidade.bairro, 
+                             obj_chamado_goon.localidade.cidade.name, 
+                             obj_chamado_goon.localidade.estado.code, 
+                             obj_chamado_goon.localidade.cep, 
+                             obj_chamado_goon.observacao, 
+                             obj_chamado_goon.localidade.latitude, 
+                             obj_chamado_goon.localidade.longitude, 
+                             str(obj_chamado_goon.create_date).replace(" ", "T"), 
+                             '',
+                             0,
+                             '0',
+                             obj_chamado_goon.tecnico.codigo_usuario,
+                             str(obj_chamado_goon.data).replace(" ", "T"),
+                             '',
+                             'Internal'))
+
+        if retorno['success']:
+            obj_chamado_goon.write({'name': retorno['numeroOS'], 'status': 'DESP'})
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'action_warn',
+                'name': 'Warning',
+                'params': {
+                    'title': 'Sincronização Concluída!',
+                    'text': 'Dados enviados para o Goon.',
+                    'sticky': True
+                    }
+            }
+        else:
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'action_warn',
+                'name': 'Warning',
+                'params': {
+                    'title': 'Erro ao sincronizar!',
+                    'text': 'Houve um erro ao sincronizar, contate o administrador do sistema.',
+                    'sticky': False
+                    }
+            }
+            
+    def editarOrdemServico(self, obj_companhia, obj_chamado_goon, obj_agenda_tecnicos):
+        client = Client(obj_companhia.url)
+        retorno = json.loads(client.service.SetAcompanhamentoOrdemServico(
+                             'A7E43A2C4974A8282B6E6754AF5F06DCA0DE988B', 
+                             obj_companhia.client_code, 
+                             obj_chamado_goon.id,
+                             2,
+                             obj_agenda_tecnicos.name.codigo_usuario,
+                             str(obj_agenda_tecnicos.data).replace(" ", "T"),
+                             obj_chamado_goon.status,
+                             'true',
+                             'Internal',
+                             0))
+
+        if retorno['success']:
+            obj_chamado_goon.write({'data': obj_agenda_tecnicos.data, 'tecnico': obj_agenda_tecnicos.name.id})
+            return obj_chamado_goon.status
+        
+    def acompanharOrdemServico(self, obj_companhia, obj_chamado_goon, obj_agenda_tecnicos):
+        client = Client(obj_companhia.url)
+        retorno = json.loads(client.service.GetAcompanhamentoOrdemServico(
+                             'B2754231E374B8DAB17B09DA83E40AF1354D3EFD', 
+                             obj_companhia.client_code, 
+                             obj_chamado_goon.id))
+
+        if retorno['success']:
+            obj_chamado_goon.write({'status': retorno['status']})
+            obj_agenda_tecnicos.write({'status_goon': retorno['status']})
+            return retorno['status']
