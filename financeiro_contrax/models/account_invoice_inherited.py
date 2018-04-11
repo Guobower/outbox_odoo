d9bd67d10f502b358836e1fc88a2280cd8c7420b
Edson Junior <edsondelimajunior@gmail.com>
2018-04-11 20:27:06 -0300

    Atualizando campos de pagamentos

--- financeiro_contrax/models/account_invoice_inherited.py
+++ financeiro_contrax/models/account_invoice_inherited.py
@@ -6,8 +6,10 @@ class Account_invoice_inherited(models.Model):
 
     def atualiza_campos(self, cr, uid, context=None):
         obj_faturas = self.pool.get('account.invoice').search(cr, uid, [('state','=','open'),('state','=','paid')]) 
+        print "FATURAAAAS "+str(obj_faturas)
         for nota in obj_faturas:
             fatura = self.pool.get('account.invoice').browse(cr, uid, nota)
             for item in fatura.payment_ids:
-                pagamento = self.pool.get('account.move.line').browse(cr, uid, pagamento)
+                print "PAGAMENTOOOS "+str(pagamento)
+                pagamento = self.pool.get('account.move.line').browse(cr, uid, item)
                 pagamento.write({'centro_custo': fatura.x_centro_custo.id, 'natureza_financeira': fatura.x_natureza_financeira.id})
