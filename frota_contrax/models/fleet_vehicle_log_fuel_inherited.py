6f34d09c1a574027615479b86f29fbd6dfd2c74c
Edson Junior <edsondelimajunior@gmail.com>
2018-05-26 10:26:30 -0300

    Modulo de frota com validacao de abastecimento

---
+++ frota_contrax/models/fleet_vehicle_log_fuel_inherited.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from openerp import api
+from openerp import fields
+from openerp import models
+
+class Fleet_vehicle_log_fuel_inherited(models.Model):
+    _inherit = 'fleet.vehicle.log.fuel'
+    
+    active = fields.Boolean(
+                            string="Validado",
+                            help="Registro de combustível validado ou não, pode ser bloqueado devido a um registro de combustivel anterior com maior odometro do que o atual",
+                            track_visibility='onchange'
+                            )
+    
+    def on_change_odometro(self, cr, user, ids, odometro, context=None):
+        if odometro:
+            obj_abastecimentos = self.pool.get('fleet.vehicle.log.fuel').search(cr, uid, [('odometer', '>', odometro)])
+            if len(obj_abastecimentos) > 0:
+                res = {
+                    'value': {
+                        'active': False
+                    }
+                }
+            else:
+                res = {
+                        'type': 'ir.actions.client',
+                        'tag': 'action_warn',
+                        'name': 'Warning',
+                        'params': {
+                            'title': 'Abastecimento Validado!',
+                            'text': 'Abastecimento automaticamente validado.',
+                            'sticky': False
+                            }
+                    }
+            return res
