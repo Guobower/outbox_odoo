2da5fcf6a94ce3592d831ff7307c2dddcf5a8a5b
Edson Junior <edsondelimajunior@gmail.com>
2018-05-26 15:37:31 -0300

    Modulo de frota com validacao de abastecimento

--- frota_contrax/models/fleet_vehicle_log_fuel_inherited.py
+++ frota_contrax/models/fleet_vehicle_log_fuel_inherited.py
@@ -13,48 +13,25 @@ class Fleet_vehicle_log_fuel_inherited(models.Model):
                             )
     
     def on_change_odometro(self, cr, uid, ids, odometro, veiculo, context=None):
-        print "ENTREI"
         if odometro and veiculo:
             obj_veiculo = self.pool.get('fleet.vehicle').browse(cr, uid, veiculo)
             if obj_veiculo.odometer >= odometro:
-                '''
                 res = {
                     'value': {
                         'active': False
                     }
                 }
-                '''
-                res = {
-                    'type': 'ir.actions.client',
-                    'tag': 'action_warn',
-                    'name': 'Warning',
-                    'params': {
-                        'title': 'Abastecimento não validado!',
-                        'text': 'Abastecimento automaticamente validado.',
-                        'sticky': True
-                        }
-                }
             else:
                 res = {
-                    'type': 'ir.actions.client',
-                    'tag': 'action_warn',
-                    'name': 'Warning',
-                    'params': {
-                        'title': 'Abastecimento Validado!',
-                        'text': 'Abastecimento automaticamente validado.',
-                        'sticky': True
-                        }
+                    'value': {
+                        'active': True
+                    }
                 }
         else:
             res = {
-                'type': 'ir.actions.client',
-                'tag': 'action_warn',
-                'name': 'Warning',
-                'params': {
-                    'title': 'Nem entrou!' + str(odometro),
-                    'text': 'Abastecimento automaticamente validado.' + str(veiculo),
-                    'sticky': True
-                    }
+                'value': {
+                    'active': False
+                }
             }
         
         return res
