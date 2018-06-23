c40c307e8b28077139a18a7c903c1c1b5d995b94
Edson Junior <edsondelimajunior@gmail.com>
2018-06-23 11:15:28 -0300

    Iniciando modulo de planejamento Contrax

---
+++ planejamento_contrax/__openerp__.py
@@ -0,0 +1,15 @@
+#coding=utf-8
+
+{
+    'name': 'Planejamento Contrax',
+    'category': 'Uncategorized',
+    'summary': 'Modulo de planejamento para a Contrax',
+    'version': '1.1',
+    'description': "Modulo de planejamento para a Contrax",
+    'author': 'Edson Junior',
+    'depends': ['base','account','financeiro_contrax'],
+    'installable': True,
+    'data': [
+        'views/menu.xml'
+    ]
+}
---
+++ planejamento_contrax/models/__init__.py
@@ -0,0 +1,2 @@
+from . import centro_custo_inherited
+
