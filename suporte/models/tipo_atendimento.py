72688afa834952c28e9fe64477d2f13bb0fdd8cc
edsonjunior <edsonjunior@10.1.201.105>
2018-07-25 09:09:12 -0300

    Chamado solicitando inclusao de campo ativo nas configuracoes do atendimento

--- suporte/models/tipo_atendimento.py
+++ suporte/models/tipo_atendimento.py
@@ -16,6 +16,11 @@ class Tipo_atendimento(models.Model):
         string="Descricao",
         help="Descrição",
         track_visibility="onchange")
+
+    active = fields.Boolean(
+        string="Ativo",
+        track_visibility='onchange'
+    )
     
     grupo_atendimento = fields.Many2one(
         string="Grupo de Atendimento",
